9eaeda50bfe8c01cbc84765382320abec388f4ce
FUJIIH <hideaki.fujii@dell.com>
2021-12-06 19:43:25 +0900

    final code with MASK

--- finalMASK.py
+++ finalMASK.py
@@ -2,13 +2,17 @@
 
 import os
 import time
-
+import datetime
+import requests
 #--------------------------------------------------------------
 #  Note: ds18b20's data pin must be connected to pin7(GPIO4).
 #--------------------------------------------------------------
 
 # Reads temperature from sensor and prints to stdout
 # id is the id of the sensor
+
+
+
 def readSensor(id):
     tfile = open("/sys/bus/w1/devices/"+id+"/w1_slave")
     text = tfile.read()
@@ -17,9 +21,41 @@ def readSensor(id):
     temperaturedata = secondline.split(" ")[9]
     temperature = float(temperaturedata[2:])
     temperature = temperature / 1000
-#    print ("Sensor: " + id  + " - Current temperature : %0.3f C" % temperature)
-    print (temperature)
+    now = datetime.datetime.now() 
+    print (now.strftime("%Y/%m/%d %H:%M:%S")), temperature
 
+#def send_line(text):
+    url ='https://notify-api.line.me/api/notify'
+    token = "xxxxxxxxxxxxxxxxxxxxxxx"
+    headers ={'Authorization' : 'Bearer ' + token}
+#    message = text
+    if temperature >= 27:  
+
+        message = temperature
+ #   else:
+ #       message = "normal"
+        payload = {'message' : message}
+        p = requests.post(url, headers=headers, data=payload)
+        print(p)
+    else:
+        pass
+
+#    now = datetime.datetime.now() 
+
+#text="(now)"
+#text=temperature
+#    text= "temperature, 25.684" 
+#text = "CDF" 
+#    send_line(text)
+
+
+
+#url = "https://notify-api.line.me/api/notify"
+#token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#headers = {"Authorization" : "Bearer "+ token}
+#message =  temperature, (now.strftime("%Y/%m"))
+#payload = {"message" :  message}
+#r = requests.post(url, headers = headers, params=payload) 
 
 
 # Reads temperature from all sensors found in /sys/bus/w1/devices/
